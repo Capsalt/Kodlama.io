618878a48171dcaa155ff64a8a268566f957721e
Capsalt <hbasri2304@gmail.com>
2021-11-29 22:49:49 +0300

    .

---
+++ nortwind/src/main/java/io/nortwind/core/utilities/results/Result.java
@@ -0,0 +1,24 @@
+package io.nortwind.core.utilities.results;
+
+public class Result {
+
+	private boolean success;
+	private String message;
+	
+	public Result(boolean success) {
+		
+	}
+
+	public Result(boolean success, String message) {
+		this.success= success;
+		this.message= message;
+	}
+	
+	public boolean isSuccess() {
+		return this.success;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+}
